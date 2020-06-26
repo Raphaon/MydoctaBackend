b2d53d9be098d8c460b6311eb1542dc5acb1bfa0
Rapha Ondobo <raphaondobo@gmail.com>
2020-06-26 16:08:09 +0100

    correction des appel

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
+
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 
@@ -15,7 +15,7 @@ Route::get('{apiKey}/policy', [
     'uses' => 'ApiAuthController@policy'
 ]);
 //requete des informations concernant le patient
-Route::get('/{apiKey}/patients', [
+Route::get('{apiKey}/patients', [
     'as'   => 'patientList',
     'uses' => 'PatientController@index'
 ]);
