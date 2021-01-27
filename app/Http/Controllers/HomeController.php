a0dcc194952469c0aca54d92344ba1ccbc2293d7
Raphaon <raphaondobo@gmail.com>
2021-01-27 17:01:27 +0100

    Mise à jour de la home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\api_user;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return view("Home/Index");
+        return view("Home/home");
     }
 
-    public function getAppList()
+    /* public function getAppList()
     {
         return api_user::get();
-    }
+    }*/
 }
