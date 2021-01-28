5d69af188c09896b4363d46f4a6e14d19595a53b
Raphaon <raphaondobo@gmail.com>
2021-01-28 08:12:57 +0100

    Mise à jour de la home page

---
+++ resources/views/Layouts/Home/Home_Partials/__foot.blade.php
@@ -0,0 +1,50 @@
+ <!-- Scroll Up -->
+ <div id="back-top" >
+    <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+</div>
+
+<!-- Js here -->
+
+<script src="./Assets/Js/vendor/modernizr-3.5.0.min.js"></script>
+<!-- Jquery, Popper, Bootstrap -->
+<script src="./Assets/Js/vendor/jquery-1.12.4.min.js"></script>
+<script src="./Assets/Js/popper.min.js"></script>
+<script src="./Assets/Js/bootstrap.min.js"></script>
+<!-- Jquery Mobile Menu -->
+<script src="./Assets/Js/jquery.slicknav.min.js"></script>
+
+<!-- Jquery Slick , Owl-Carousel Plugins -->
+<script src="./Assets/Js/owl.carousel.min.js"></script>
+<script src="./Assets/Js/slick.min.js"></script>
+<!-- One Page, Animated-HeadLin -->
+<script src="./Assets/Js/wow.min.js"></script>
+<script src="./Assets/Js/animated.headline.js"></script>
+<script src="./Assets/Js/jquery.magnific-popup.js"></script>
+
+<!-- Date Picker -->
+<script src="./Assets/Js/gijgo.min.js"></script>
+<!-- Nice-select, sticky -->
+<script src="./Assets/Js/jquery.nice-select.min.js"></script>
+<script src="./Assets/Js/jquery.sticky.js"></script>
+<!-- Progress -->
+<script src="./Assets/Js/jquery.barfiller.js"></script>
+
+<!-- counter , waypoint,Hover Direction -->
+<script src="./Assets/Js/jquery.counterup.min.js"></script>
+<script src="./Assets/Js/waypoints.min.js"></script>
+<script src="./Assets/Js/jquery.countdown.min.js"></script>
+<script src="./Assets/Js/hover-direction-snake.min.js"></script>
+
+<!-- contact js -->
+<script src="./Assets/Js/contact.js"></script>
+<script src="./Assets/Js/jquery.form.js"></script>
+<script src="./Assets/Js/jquery.validate.min.js"></script>
+<script src="./Assets/Js/mail-script.js"></script>
+<script src="./Assets/Js/jquery.ajaxchimp.min.js"></script>
+
+<!-- Jquery Plugins, main Jquery -->
+<script src="./Assets/Js/plugins.js"></script>
+<script src="./Assets/Js/Main.js"></script>
+
+</body>
+</html>
